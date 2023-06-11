46510face9bf805a01b8501f8185ba85bdb7d19e
wllfore <94956181+wllfore@users.noreply.github.com>
2023-06-11 12:31:16 +0800

    Update run_extract_feats.sh

--- run_extract_feats.sh
+++ run_extract_feats.sh
@@ -1,2 +1,3 @@
-python extract_feats.py --model simmim-swin-base --dataset Endo --data_dir /mnt/data/smart_health_02/wanglilong/data/MedFMC/Endo
-python extract_feats.py --model swin-base --dataset Endo --data_dir /mnt/data/smart_health_02/wanglilong/data/MedFMC/Endo
+### sample script of extract features of test set in MedFMC
+python ./code/extract_feats.py --model simmim-swin-base --dataset Endo --data_dir ***/MedFMC/Endo
+python ./code/extract_feats.py --model swin-base --dataset Endo --data_dir ***/MedFMC/Endo
