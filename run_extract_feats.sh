3e10632eee6789d9568572eb2ce97f4c93b0e3ad
wllfore <94956181+wllfore@users.noreply.github.com>
2023-06-12 16:56:23 +0800

    Update run_extract_feats.sh

--- run_extract_feats.sh
+++ run_extract_feats.sh
@@ -1,3 +1,3 @@
 ### sample script of extract features of test set in MedFMC
-python ./baseline/extract_feats.py --model simmim-swin-base --dataset ChestDR --data_dir ***/MedFMC/ChestDR
-python ./baseline/extract_feats.py --model swin-base --dataset ChestDR --data_dir ***/MedFMC/ChestDR
+python ./baseline/extract_feats.py --model simmim-swin-base --dataset ChestDR --data_dir ./data/MedFMC/ChestDR
+python ./baseline/extract_feats.py --model swin-base --dataset ChestDR --data_dir ./data/MedFMC/ChestDR
