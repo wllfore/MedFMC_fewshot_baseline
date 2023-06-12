2de9877200acd191acda2ba5af161d6e4126c014
wllfore <94956181+wllfore@users.noreply.github.com>
2023-06-12 13:53:39 +0800

    Update run_fewshot_baseline.sh

--- run_fewshot_baseline.sh
+++ run_fewshot_baseline.sh
@@ -1,3 +1,3 @@
 ### sample script of run fewshot baseline on test set of MedFMC
-python fewshot_baseline.py --method Baseline --model swin-base --dataset ChestDR --data_dir ***/MedFMC/ChestDR --shot 10 --max_iters 10 --job_name test_shot10_base
-python fewshot_baseline.py --method MetaBaseline --model swin-base --dataset ChestDR --data_dir ***/MedFMC/ChestDR --shot 10 --max_iters 10 --job_name test_meta_shot10_base
+python ./code/fewshot_baseline.py --method Baseline --model swin-base --dataset ChestDR --data_dir ***/MedFMC/ChestDR --shot 10 --max_iters 10 --job_name test_shot10_base
+python ./code/fewshot_baseline.py --method MetaBaseline --model swin-base --dataset ChestDR --data_dir ***/MedFMC/ChestDR --shot 10 --max_iters 10 --job_name test_meta_shot10_base
